e02f6ebdffc7a3e69638f3352687246ac478f521
Nicolás Arias <josue.arias@epn.edu.ec>
2023-07-01 03:05:56 -0500

    Update db.py

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ from py2neo import Graph
 from tfidf import Model
 
 def getDB():
-    return Graph("bolt://3.138.37.108:7687", auth=("neo4j", "narias98"))
+    return Graph("bolt://52.146.3.93:7687", auth=("neo4j", "narias98"))
 
 def getAuthorsByQuery(name, page, size):
 
